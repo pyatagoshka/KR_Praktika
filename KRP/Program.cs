acb77b765abd9bfbe92a3841579f14995d4e678f
pytya <33357187+pyatagoshka@users.noreply.github.com>
2017-11-16 20:24:53 +0300

    first commit

---
+++ KRP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            ConsoleKeyInfo k;
+            int x = 80;
+            int y = 40;
+            do
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write((char)2);
+                k = Console.ReadKey(true);
+                if (k.Key == ConsoleKey.LeftArrow && x > 0)
+                    x--;
+                if (k.Key == ConsoleKey.DownArrow && y < 80)
+                    y++;
+                if (k.Key == ConsoleKey.UpArrow && y > 0)
+                    y--;
+                if (k.Key == ConsoleKey.RightArrow && x < 160)
+                    x++;
+                Console.Clear();
+            }
+            while (k.Key != ConsoleKey.Escape);
+        }
+    }
+}
